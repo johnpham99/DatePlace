5057da3d54c6ef2d64a514664dea8c0a00758761
John Pham <54965720+johnpham99@users.noreply.github.com>
2022-07-09 10:35:32 -0400

    add error handling/data validation

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const path = require("path");
 const Dateplace = require("./models/dateplace");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
+const { dateplaceSchema } = require("./schemas.js")
 
 
 const mongoose = require("mongoose");
@@ -20,7 +24,6 @@ db.once("open", () => {
 
 
 
-
 const app = express();
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
@@ -29,45 +32,65 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validateDateplace = (req, res, next) => {
+    const { error } = dateplaceSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/dateplaces", async (req, res) => {
+app.get("/dateplaces", catchAsync(async (req, res, next) => {
     const dateplaces = await Dateplace.find({});
     res.render("dateplaces/index", { dateplaces });
-})
+}))
 
-app.post("/dateplaces", async (req, res) => {
+app.post("/dateplaces", validateDateplace, catchAsync(async (req, res, next) => {
     const dateplace = new Dateplace(req.body.dateplace);
     await dateplace.save();
     res.redirect(`/dateplaces/${dateplace._id}`);
-})
+}))
 
 app.get("/dateplaces/new", (req, res) => {
     res.render("dateplaces/new");
 })
 
-app.get("/dateplaces/:id", async (req, res) => {
+app.get("/dateplaces/:id", catchAsync(async (req, res, next) => {
     const dateplace = await Dateplace.findById(req.params.id);
     res.render("dateplaces/show", { dateplace });
-})
+}))
 
-app.get("/dateplaces/:id/edit", async (req, res) => {
+app.get("/dateplaces/:id/edit", catchAsync(async (req, res, next) => {
     const dateplace = await Dateplace.findById(req.params.id);
     res.render("dateplaces/edit", { dateplace });
-})
+}))
 
-app.put("/dateplaces/:id", async (req, res) => {
+app.put("/dateplaces/:id", validateDateplace, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const dateplace = await Dateplace.findByIdAndUpdate(id, { ...req.body.dateplace })
     res.redirect(`/dateplaces/${dateplace._id}`);
-})
+}))
 
-app.delete("/dateplaces/:id", async (req, res) => {
+app.delete("/dateplaces/:id", catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Dateplace.findByIdAndDelete(id);
     res.redirect("/dateplaces");
+}))
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404));
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Something went wrong"
+    res.status(statusCode).render("error", { err })
 })
 
 app.listen(3000, () => {
