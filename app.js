79ed66b09d8a484c9e80313e2eb8898abd82e885
John Pham <54965720+johnpham99@users.noreply.github.com>
2022-07-10 17:12:20 -0400

    configure session

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const path = require("path");
-const Dateplace = require("./models/dateplace");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
-const { dateplaceSchema, reviewSchema } = require("./schemas.js");
-const Review = require("./models/review");
+//const Joi = require("joi");
 
 const mongoose = require("mongoose");
-const dateplace = require("./models/dateplace");
 mongoose.connect("mongodb://localhost:27017/date-place", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -30,7 +27,18 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
 
 const dateplaces = require("./routes/dateplaces");
 const reviews = require("./routes/reviews");
